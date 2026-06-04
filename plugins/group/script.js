36c74294eb949b2dc9b65d516ad6fc53858c6028
phopadd0-lab <phopadd0@gmail.com>
2026-06-04 14:36:53 +0300

    Update script.js

--- plugins/group/script.js
+++ plugins/group/script.js
@@ -7,12 +7,12 @@ const context = (jid, img) => ({
     isForwarded: true,
     forwardingScore: 1,
     forwardedNewsletterMessageInfo: {
-        newsletterJid: '120363225356834044@newsletter',
-        newsletterName: '𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️',
+        newsletterJid: '201556853817@newsletter',
+        newsletterName: '‌🇦‌🇱‌🇭‌🇼‌🇦‌🇷‌🇾',
         serverMessageId: 0
     },
     externalAdReply: {
-        title: "𝐏𝐎𝐌𝐍𝐈-𝐀𝐈 🎪 | 𝐁𝐨𝐭 𝐢𝐬 𝐛𝐮𝐢𝐥𝐭 𝐨𝐧 𝐭𝐡𝐞 𝐖𝐒/𝐕𝐈𝐈 𝐟𝐫𝐚𝐦𝐞𝐰𝐨𝐫𝐤",
+        title: "‌🇦‌🇱‌🇭‌🇼‌🇦‌🇷‌🇾",
         body: "𝚆𝚑𝚊𝚝𝚜𝙰𝚙𝚙 𝚋𝚘𝚝 𝚝𝚑𝚊𝚝 𝚒𝚜 𝚎𝚊𝚜𝚢 𝚝𝚘 𝚖𝚘𝚍𝚒𝚏𝚢 𝚊𝚗𝚍 𝚟𝚎𝚛𝚢 𝚏𝚊𝚜𝚝",
         thumbnailUrl: img,
         sourceUrl: '',
@@ -24,11 +24,11 @@ const { images } = bot.config.info;
 const img = images.random()
 await conn.sendMessage(m.chat, { 
   text: `
-GitHub: _*https://github.com/deveni0/Pomni-AI/tree/main*_
+GitHub: _* ابعد عن الكلام ده يا عرصٍ*_
 
-Video: _*https://youtu.be/hA5aCpvesJE?si=pHAEsbDFTVXe2_sq*_
+Video: _* يا خۆلُ بتجيب لنفسك التهزيق*_
 
-> *لا تنسي وضع نجمة لـ الريبو 🌟*
+> * ابعد ياض يا عرصٍ من هنا 🌟*
 `,
   contextInfo: context(m.sender, img)
 }, { quoted: reply_status });
