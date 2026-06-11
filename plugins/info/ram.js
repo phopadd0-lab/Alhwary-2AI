e9fa1a78b989d5ec9711d959afb4f2c4ca5fd975
phopadd0-lab <phopadd0@gmail.com>
2026-06-11 13:20:18 +0300

    Update ram.js

--- plugins/info/ram.js
+++ plugins/info/ram.js
@@ -4,7 +4,7 @@ const handler = async (m, { conn }) => {
   const txt = `
   ⊱⋅ ────────────────── ⋅⊰
 🪾╎ الـمسـتـخـدم: ${(process.memoryUsage().rss / 1024 / 1024).toFixed(1)}MB
-🌳╎ الـمتـبــقـي: ${(os.freemem() / 1024 / 1024).toFixed(1)}MB
+💻╎ الـمتـبــقـي: ${(os.freemem() / 1024 / 1024).toFixed(1)}MB
   ⊱⋅ ────────────────── ⋅⊰
 `;
 
@@ -14,8 +14,8 @@ const handler = async (m, { conn }) => {
     title: "𝐒𝐩𝐞𝐞𝐝 / 𝐓𝐞𝐬𝐭",
     body: "𝐓𝐞𝐬𝐭𝐢𝐧𝐠 𝐭𝐡𝐞 𝐛𝐨𝐭'𝐬 𝐬𝐩𝐞𝐞𝐝: 𝐈𝐬 𝐢𝐭 𝐟𝐚𝐬𝐭 𝐨𝐫 𝐧𝐨𝐭?",
     newsletter: {
-      name: '𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️',
-      jid: '120363225356834044@newsletter'
+      name: ' 𝐴𝐿𝐻𝑊𝐴𝑅𝑌 💻',
+      jid: '201556853817@newsletter'
     },
     big: false,
     mentions: [m.sender]
