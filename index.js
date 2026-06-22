3cc609b4ee96256984531c85231290a511bf7ba0
phopadd0-lab <phopadd0@gmail.com>
2026-06-22 22:58:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import sub from './sub.js';
 
 /* =========== Client ========== */
 const client = new Client({
-  phoneNumber:'584266318838', // Bot number
+  phoneNumber:'962770922356', // Bot number
   prefix: [".", "/", "!"],
   fromMe: false, 
   owners: [
