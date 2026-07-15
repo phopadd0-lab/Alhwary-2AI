63ec32eb3885cb3868db00f8b7a24aa0fd09d7f9
phopadd0-lab <phopadd0@gmail.com>
2026-07-15 18:45:39 +0300

    Update add_owner.js

--- plugins/owner/add_owner.js
+++ plugins/owner/add_owner.js
@@ -1,24 +1,28 @@
 const handler = async (m, { conn, bot }) => {
   let targetLid = m.mentionedJid?.[0] || m.quoted?.sender;
-  let targetJid = m.lid2jid(m.mentionedJid?.[0] || m.quoted?.sender)
-   if (!targetJid || !targetLid) {
+  let targetJid = m.lid2jid(m.mentionedJid?.[0] || m.quoted?.sender);
+
+  if (!targetJid || !targetLid) {
     return m.reply('⚠️ *يرجى منشن الشخص أو الرد على رسالته* ⚠️');
   }
+
   const user = (await conn.groupMetadata(m.chat)).participants.find(
-            p => p.id === targetLid || 
-                 p.id === m.sender ||
-                 p.phoneNumber === targetJid
-        );
+    p => p.id === targetLid || 
+         p.id === m.sender ||
+         p.phoneNumber === targetJid
+  );
+
   await bot.config.owners.push({
-       name: "Owner",
-       jid: user.phoneNumber,
-       lid: user.id
-      })
-   m.reply("📂: تم اضافة مطور جديد")
+    name: "𝐴𝐿𝐻𝑊𝐴𝑅𝑌 🔥",
+    jid: user.phoneNumber,
+    lid: user.id
+  });
+
+  m.reply("📂: تم إضافة مطور جديد بواسطة 𝐴𝐿𝐻𝑊𝐴𝑅𝑌 🔥");
 };
 
 handler.usage = ["اضافه-مطور"];
-handler.category = "owner";
+handler.category = "𝐴𝐿𝐻𝑊𝐴𝑅𝑌 🔥";
 handler.command = ["ضيف_مطور", "اضافه_مطور"];
 handler.owner = true;
 
