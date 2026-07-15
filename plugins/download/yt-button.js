1ee55023db86aa3655c2354c597cce7952cf5713
phopadd0-lab <phopadd0@gmail.com>
2026-07-15 18:50:55 +0300

    Update yt-button.js

--- plugins/download/yt-button.js
+++ plugins/download/yt-button.js
@@ -1,6 +1,9 @@
+import fs from 'fs';
+import path from 'path';
+
 const handler = async (m, { conn, text }) => {
-    if (!text) return m.reply("💙 ~ اكتب نص الفيديو او الاغنيه ~ ❤️");
-    
+    if (!text) return m.reply("💙 ~ اكتب نص الفيديو أو الأغنية ~ ❤️");
+
     const res = await fetch(`https://emam-api.web.id/home/sections/Search/api/YouTube/search?q=${text}`);
     const { data } = await res.json();
     const { title, image, timestamp: time, url } = data[0];
@@ -10,17 +13,17 @@ const handler = async (m, { conn, text }) => {
         bodyText: `${title} ╎ ${time}`,
         footerText: "🕸️ 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 ~ 𝐘𝐨𝐮𝐓𝐮𝐛𝐞 🕸️",
         buttons: [
-            { name: "quick_reply", params: { display_text: "🎼 ╎ تـحـمـيـل صـوت", id: `.يوت_اغنيه ${url}` } },
-            { name: "quick_reply", params: { display_text: "🎬 ╎ تـحـمـيـل فـيـديـو", id: `.يوتيوب ${url}` } }
+            { name: "quick_reply", params: { display_text: "🎼 ╎ تحميل صوت", id: `.يوت_اغنيه ${url}` } },
+            { name: "quick_reply", params: { display_text: "🎬 ╎ تحميل فيديو", id: `.يوتيوب ${url}` } }
         ],
         mentions: [m.sender],
-        newsletter: { name: "O̷W̷N̷E̷R̷ | ڵــﮪــﯡٰڕې", jid: "201556853817@newsletter" },
+        newsletter: { name: "𝐴𝐿𝐻𝑊𝐴𝑅𝑌 🔥", jid: "201556853817@newsletter" },
         interactiveConfig: { buttons_limits: 10, list_title: "", button_title: "", canonical_url: url }
     }, m);
 };
 
 handler.usage = ["فيديو", "اغنيه", "شغل"];
-handler.category = "downloads";
+handler.category = "𝐴𝐿𝐻𝑊𝐴𝑅𝑌 🔥 downloads";
 handler.command = ["اغنيه", "فيديو", "اغنية", "play", "video"];
 
 export default handler;
